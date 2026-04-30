fb4c2e78935a4d620dd025a93ae569e09c8bda5e
Alex <ahiggins@ruggedzen.com>
2026-04-29 18:40:56 -0700

    Day 2 start

---
+++ Day_2/variables.py
@@ -0,0 +1,17 @@
+'''
+Level 1
+'''
+
+#Day 2: 30 Days of python programming
+firstName = 'Rugged'
+lastName = 'Zen'
+fullName = firstName + " " + lastName
+country = 'USA'
+city = 'Flavortown'
+age = 67
+year = 1
+is_married = False
+is_true = True
+is_light_on = True
+
+#I know this and is boring
